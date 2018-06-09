6a3a93e72b03398ade8a361581798d32537f3b9e
joaofouyer <jfouyer@gmail.com>
2018-06-09 17:49:06 -0300

    removed system pause

--- src/recursao/main.c
+++ src/recursao/main.c
@@ -1,16 +1,14 @@
 #include "fibonacciRecursivo.h"
 #include "pote.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
     printf("\nQuestão 1. a): %d\n", Pote(6,3));
     printf("\nQuestão 1. b): %d\n", Fat(6));
     printf("\nQuestão 1. c): %d\n", mdc1(24, 16));
     printf("\nQuestão 1. d): %d\n", mdc2(24, 16));
-    printf("Questão 2.: %d\n", fib2(10));
-    printf("Questão 3.: %d\n", X(4));
-    printf("Questão 4.: %d\n", XX(4));
-    system("PAUSE");
+    printf("\nQuestão 2.: %d\n", fib2(10));
+    printf("\nQuestão 3.: %d\n", X(4));
+    //printf("\nQuestão 4.: %d\n", XX(4));
     return 0;
 }
